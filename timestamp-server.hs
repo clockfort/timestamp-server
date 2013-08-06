ae125d2cc5f1fb64771128acac1a1ec51b265ce1
Chris Lockfort <clockfort@csh.rit.edu>
2013-08-06 02:38:57 -0400

    port 80, for easy querying

--- timestamp-server.hs
+++ timestamp-server.hs
@@ -4,15 +4,19 @@ import Snap.Core
 import Snap.Http.Server
 import Data.Global
 import Data.Text hiding (map, concat, head, last, zip)
+import qualified Data.ByteString.Char8 as B
 
 increment :: TVar Int -> STM ()
 increment counter = do
 	x <- readTVar counter
 	writeTVar counter (x + 1)
 
-main = quickHttpServe $ ifTop atomicGetTimestamp
+main = httpServe config $ ifTop atomicGetTimestamp
+
+config = setBind (B.pack "0.0.0.0") . setPort 80 $ defaultConfig
 
 atomicGetTimestamp = do
 	let timestamp = declareTVar "timestamp" 0
 	counter <- liftIO $ atomically (increment timestamp)
-	writeText $ pack $ show counter
+	current <- liftIO $ atomically (readTVar timestamp)
+	writeText $ pack $ show current 
